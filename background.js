fa5edeffe0e369d6945d228e28fce0fc2dc91740
Bob Jiang <bob.jiang@orderplus.com>
2019-07-10 14:13:06 +0800

    add janbao change theme, add search box

--- background.js
+++ background.js
@@ -1,11 +1,16 @@
-const filter = {
-    url:
-    [
-      {hostContains: "janbao.net"},
-    ]
-  }
+// const filter = {
+//     url:
+//     [
+//       {hostContains: "janbao.net"},
+//     ]
+//   }
   
-browser.webNavigation.onDOMContentLoaded.addListener(function (details) {
-    console.log('onDOMContentLoaded :', details.url);
-}, filter)
-console.log("background.js")
+// browser.webNavigation.onDOMContentLoaded.addListener(function (details) {
+//     console.log('onDOMContentLoaded :', details.url);
+// }, filter)
+// console.log("background.js")
+
+
+
+// var insertingCSS = browser.tabs.insertCSS({file: "janbao-dark-theme.css"});
+// insertingCSS.then(null, console.error);
--- janbao.js
+++ janbao.js
@@ -1,5 +1,3 @@
-console.log("janbao");
-
 function ready(fn) {
   if (
     document.attachEvent
@@ -12,45 +10,51 @@ function ready(fn) {
   }
 }
 
-// function addSearchBox() {
-//   var searchBox, whoisonlinebox;
-//   searchBox = document.createElement("div");
-//   whoisonlinebox = document.getElementById("Panel");
-//   mobilebox = document.getElementsByClassName("Banner");
-//   if (whoisonlinebox) {
-//     whoisonlinebox.appendChild(searchBox);
-//     searchBox.innerHTML =
-//       '<div id="SearchBox" class="Box"><h4>搜索</h4><form method=get action="https://www.google.com.hk/search"><input class="DateBox" type=text name=q><input class="Button hide" type=submit name=btnG value="走你"><input type=hidden name=ie value=UTF-8><input type=hidden name=oe value=UTF-8><input type=hidden name=hl value=zh-CN><input type=hidden name=domains value="huox.in"><input type=hidden name=sitesearch value="huox.in"></form></div>';
-//   } else {
-//     mobilebox[0].appendChild(searchBox);
-//     searchBox.innerHTML =
-//       '<div id="SearchBox" class="Box"><form method=get action="https://www.google.com.hk/search"><a><b>搜索:</b></a><input class="DateBox" type=text name=q><input class="Button hide" type=submit name=btnG value="走你"><input type=hidden name=ie value=UTF-8><input type=hidden name=oe value=UTF-8><input type=hidden name=hl value=zh-CN><input type=hidden name=domains value="huox.in"><input type=hidden name=sitesearch value="huox.in"></form></div>';
-//   }
-// }
+function addSearchBox() {
+  const panel = document.getElementById('Panel');
+  const box = document.getElementById('SearchBox');
+  const searchBox = `<div class="Box" id="SearchBox"><h4>搜索：</h4><form method="get" action="/search">
+                      <input type="text" id="Form_Search" name="Search" value="" placeholder="按 Enter 搜索" class="InputBox">
+                    </form></div>`
+  if(panel && !box){
+    panel.insertAdjacentHTML('beforeend', searchBox);
 
-function bindUserStyle(){
-    const theme = localStorage.getItem('theme')
-    if(!theme) {
-        localStorage.setItem('theme','default')
-    }
+  }
+}
 
-    if(document.querySelector('#theme')){
-      document.querySelectorAll("#Head .Row")[0].insertAdjacentHTML('beforeend', '<ul id="theme" class="SiteMenu"><li><a class="default">原版</a></li><li><a class="dark">夜间</a></li></ul>');
-    }
-    
-    // if(_temp === null || _temp === "default"){
-    //     $("[user-style=true]").attr("disabled",true);
-    // } else {
-    //     $("."+_temp).attr("disabled",false);
-    // }
-    // $(".right-menu li a").live("click",function(){
-    //     var type = $(this).attr("class");
-    //     $("[user-style=true]").attr("disabled",true);
-    //     $("."+type).attr("disabled",false);
-    // });  
+function bindUserStyle() {
+  const html = document.querySelector("html");
+  const theme = localStorage.getItem("theme");
+
+  if (theme) {
+    html.classList.add(theme);
+  } else {
+    localStorage.setItem("theme", "default");
+  }
+
+  if (!document.querySelector("#theme")) {
+    document
+      .querySelectorAll("#Head .Row")[0]
+      .insertAdjacentHTML(
+        "beforeend",
+        '<ul id="theme" class="SiteMenu"><li><a data-theme="default">原版</a></li><li><a data-theme="dark">夜间</a></li></ul>'
+      );
+  }
+
+  document.querySelectorAll("#theme a").forEach(el => {
+    el.addEventListener("click", () => {
+      const currentTheme = localStorage.getItem("theme");
+      const theme = el.getAttribute("data-theme");
+      if (currentTheme !== theme) {
+        html.classList.remove(currentTheme);
+        html.classList.add(theme);
+        localStorage.setItem("theme", theme);
+      }
+    });
+  });
 }
 
 ready(() => {
-    bindUserStyle()
-//   addSearchBox();
+  bindUserStyle();
+  addSearchBox();
 });
