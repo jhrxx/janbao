4beda5383c50ff4d4765163dfeb94331cf55f676
Bob Jiang <bob.jiang@orderplus.com>
2019-07-12 07:21:20 +0800

    removed uesless images

--- jandan.js
+++ jandan.js
@@ -1,1 +1,36 @@
-console.log('jandan');
+console.log("jandan");
+function ready(fn) {
+  if (
+    document.attachEvent
+      ? document.readyState === "complete"
+      : document.readyState !== "loading"
+  ) {
+    fn();
+  } else {
+    document.addEventListener("DOMContentLoaded", fn);
+  }
+}
+
+ready(() => {
+  console.log("loaded");
+  fetch("http://jandan.net/pic/page-109#comments")
+    .then(resp => {
+      resp
+        .text()
+        .then(data => {
+          console.log("daat :", data);
+        })
+        .catch(error => {
+          console.error(error);
+        })
+        .finally(object => {
+          console.log("object :", object);
+        });
+    })
+    .catch(error => {
+      console.error(error);
+    })
+    .finally(object => {
+      console.log("object :", object);
+    });
+});
